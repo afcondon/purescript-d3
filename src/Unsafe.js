2282c39678f9e6e8d02dd7c9e09d850b4bb911f5
Andrew Condon <andrew.condon@gmail.com>
2016-03-13 18:33:32 +0100

    add foreign functions for shared module unsafe
    used by both Time.purs and Scale.purs

---
+++ src/Unsafe.js
@@ -0,0 +1,25 @@
+/* global exports */
+"use strict";
+
+// module Graphics.D3.Unsafe
+
+exports.unsafeDomainImpl    = domain
+exports.unsafeRangeImpl     = range
+exports.unsafeCopy          = copy
+exports.unsafeToFunction    = toFunction
+
+function domain(domain, scale) {
+  return scale.domain(domain);
+}
+
+function range(values, scale) {
+  return scale.range(values);
+}
+
+function copy(scale) {
+  return scale.copy();
+}
+
+function toFunction(scale) {
+  return scale.copy();          // is that really a D3 function? check TODO
+}
--- src/Unsafe.purs
+++ src/Unsafe.purs
@@ -5,11 +5,21 @@ module Graphics.D3.Unsafe (
 	unsafeToFunction
   ) where
 
-import Data.Foreign.EasyFFI
+import Graphics.D3.Base
+import Control.Monad.Eff
+import Data.Function.Eff
+-- import Data.Foreign.EasyFFI
 
-ffi = unsafeForeignFunction
+foreign import unsafeDomainImpl     :: forall s d r eff.  EffFn2 (d3::D3|eff) (Array d) (s d r) (s d r)
+foreign import unsafeRangeImpl      :: forall s d r eff.  EffFn2 (d3::D3|eff) (Array r) (s d r) (s d r)
+foreign import unsafeCopyImpl       :: forall s d r eff.  EffFn1 (d3::D3|eff) (s d r) (s d r)
+foreign import unsafeToFunctionImpl :: forall s d r eff.  EffFn1 (d3::D3|eff) (s d r) (d -> r)
 
-unsafeDomain = ffi ["domain", "scale", ""] "scale.domain(domain)"
-unsafeRange = ffi ["values", "scale", ""] "scale.range(values)"
-unsafeCopy = ffi ["scale", ""] "scale.copy()"
-unsafeToFunction = ffi ["scale", ""] "scale.copy()"
+unsafeDomain :: forall s d r eff. Array d -> s d r  -> Eff (d3::D3|eff) (s d r)
+unsafeDomain =      runEffFn2 unsafeDomainImpl
+unsafeRange :: forall s d r eff. Array r -> s d r   -> Eff (d3::D3|eff) (s d r)
+unsafeRange =       runEffFn2 unsafeRangeImpl
+unsafeCopy :: forall s d r eff. s d r               -> Eff (d3::D3|eff) (s d r)
+unsafeCopy =        runEffFn1 unsafeCopyImpl
+unsafeToFunction :: forall s d r eff. s d r         -> Eff (d3::D3|eff) (d -> r)
+unsafeToFunction =  runEffFn1 unsafeToFunctionImpl
