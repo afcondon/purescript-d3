196e94a1330731dc940b645af5ea0f1a7d1d712d
Andrew Condon <andrew.condon@gmail.com>
2016-03-13 20:56:53 +0100

    omitted from previous commit

--- src/Scale.purs
+++ src/Scale.purs
@@ -40,7 +40,7 @@ import Control.Monad.Eff
 import Data.Function.Eff
 import Graphics.D3.Base
 import Graphics.D3.Interpolate
--- import Graphics.D3.Unsafe
+import Graphics.D3.Unsafe
 
 import Data.Tuple
 import Data.Maybe
@@ -58,20 +58,6 @@ class Scale s where
   range ::      forall d r eff. Array r -> s d r -> Eff (d3::D3|eff) (s d r)
   copy ::       forall d r eff. s d r            -> Eff (d3::D3|eff) (s d r)
   toFunction :: forall d r eff. s d r            -> Eff (d3::D3|eff) (d -> r)
-
-foreign import unsafeDomainImpl     :: forall s d r eff.  EffFn2 (d3::D3|eff) (Array d) (s d r) (s d r)
-foreign import unsafeRangeImpl      :: forall s d r eff.  EffFn2 (d3::D3|eff) (Array r) (s d r) (s d r)
-foreign import unsafeCopyImpl       :: forall s d r eff.  EffFn1 (d3::D3|eff) (s d r) (s d r)
-foreign import unsafeToFunctionImpl :: forall s d r eff.  EffFn1 (d3::D3|eff) (s d r) (d -> r)
-
-unsafeDomain :: forall s d r eff. Array d -> s d r  -> Eff (d3::D3|eff) (s d r)
-unsafeDomain =      runEffFn2 unsafeDomainImpl
-unsafeRange :: forall s d r eff. Array r -> s d r   -> Eff (d3::D3|eff) (s d r)
-unsafeRange =       runEffFn2 unsafeRangeImpl
-unsafeCopy :: forall s d r eff. s d r               -> Eff (d3::D3|eff) (s d r)
-unsafeCopy =        runEffFn1 unsafeCopyImpl
-unsafeToFunction :: forall s d r eff. s d r         -> Eff (d3::D3|eff) (d -> r)
-unsafeToFunction =  runEffFn1 unsafeToFunctionImpl
 
 -- Quantitative (numeric domain) scales
 
