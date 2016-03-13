8a0a194f72ce5c9b991dc927d80aa3c06d521eb2
Andrew Condon <andrew.condon@gmail.com>
2016-03-13 21:50:37 +0100

    tiny alignments

--- src/Util.purs
+++ src/Util.purs
@@ -20,15 +20,15 @@ import Data.Function.Eff
 class Magnitude n
 
 instance numberMagnitude :: Magnitude Number
-instance dateMagnitude :: Magnitude JSDate
+instance dateMagnitude   :: Magnitude JSDate
 
 foreign import min'     :: forall d m. (Magnitude m) => (d -> m) -> Array d -> m
 foreign import max'     :: forall d m. (Magnitude m) => (d -> m) -> Array d -> m
-foreign import min      :: forall m. (Magnitude m) => Array m -> m
+foreign import min      :: forall m.   (Magnitude m) => Array m -> m
 foreign import max      :: forall d m. (Magnitude m) => Array m -> m
 -- extent takes a data array and returns [min,max]
 -- not restricted to Number, i.e. also works with time
-foreign import extent   :: forall m. (Magnitude m) => Array m -> Array m
+foreign import extent   :: forall m.   (Magnitude m) => Array m -> Array m
 foreign import extent'  :: forall d m. (Magnitude m) => (d->m) -> Array d -> Array m
 
 -- Syntactic sugar to make chained monadic statements look similar to the
