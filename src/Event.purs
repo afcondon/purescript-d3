648954f9ec917218bd61cb0f8141b9f62bffebfa
Andrew Condon <andrew.condon@gmail.com>
2016-03-21 22:42:58 +0100

    correct module name

--- src/Event.purs
+++ src/Event.purs
@@ -1,10 +1,9 @@
-module Graphics.D3.Events where
+module Graphics.D3.Event where
 
 import Prelude
 import Data.Maybe
 import Control.Monad.Eff
--- import DOM.Event.Event
 import Graphics.D3.Base
 import DOM.Event.Types
 
-foreign import currentD3Event :: forall eff. Eff(d3::D3|eff) Maybe Event
+foreign import currentD3Event :: forall eff. Eff(d3::D3|eff) (Maybe Event)
