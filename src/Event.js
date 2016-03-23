f646be1b5e366b2245d390935eb029d2b3f93b29
Andrew Condon <andrew.condon@gmail.com>
2016-03-23 16:23:11 +0100

    fixes for events

--- src/Event.js
+++ src/Event.js
@@ -8,9 +8,11 @@ var DOM = require("../DOM.Event.Event");
 // for typeclass forceGraphLayout
 exports.currentD3Event = function() {
   var possibleEvent = d3.event;
-  if (possibleEvent === null) {
-    return new Data_Maybe.Nothing();
-  } else {
-    return new Data_Maybe.Just(possibleEvent);
+  if (possibleEvent !== null) {
+    if(typeof possibleEvent.sourceEvent !== 'undefined') {
+      console.log("possibleEvent.sourceEvent is" + possibleEvent.sourceEvent);
+      return new Data_Maybe.Just(possibleEvent.sourceEvent);
+    }
   }
+  return new Data_Maybe.Nothing();
 }
--- src/Event.purs
+++ src/Event.purs
@@ -6,4 +6,10 @@ import Control.Monad.Eff
 import Graphics.D3.Base
 import DOM.Event.Types
 
+-- | Might be necessary to expose D3Event sometime but for now it suffices to pull the SourceEvent out of it
+-- | and since this is a DOM Event we can use the types defined in purescript-dom
+
+-- foreign import data D3Event :: *
+
+-- | returning a Maybe Event because the SourceEvent might be null 
 foreign import currentD3Event :: forall eff. Eff(d3::D3|eff) (Maybe Event)
